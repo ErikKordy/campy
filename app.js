882cee8549809ec4ffdc2c0da1e46b06a6f3d0b8
Erik <kordylasinski.erik@gmail.com>
2023-10-09 12:58:21 -0500

    added login & logout routes and isLoggedIn middleware

--- app.js
+++ app.js
@@ -53,13 +53,14 @@ passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
 app.use((req, res, next) => {
+    console.log(req.session)
+    res.locals.currentUser = req.user
     res.locals.success = req.flash('success')
-    next()
-})
-app.use((req, res, next) => {
     res.locals.error = req.flash('error')
     next()
 })
+
+
 app.use('/', userRoutes)
 app.use('/campgrounds', campgroundRoutes)
 app.use('/campgrounds/:id/reviews', reviewRoutes)
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const catchAsync = require('../utils/catchAsync')
+const passport = require('passport')
 const User = require('../models/user')
+const { storeReturnTo } = require('../middleware')
 
 router.get('/register', (req, res) => {
     res.render('users/register')
@@ -12,14 +14,40 @@ router.post('/register', catchAsync(async (req, res) => {
     const {email, username, password} = req.body
     const user = new User({email, username}) 
     const registeredUser = await User.register(user, password)
-    console.log(registeredUser)
-    req.flash('success', 'Welcome to Campy!')
-    res.redirect('/campgrounds')
+    req.login(registeredUser, err => {
+        if(err) return next(err)
+        req.flash('success', 'Welcome to Campy!')
+        res.redirect('/campgrounds')
+    })
     } catch (e) {
         req.flash('error', e.message)
         res.redirect('/register')
     }
 
 }))
+
+router.get('/login', (req, res) => {
+    res.render('users/login')
+})
+
+router.post('/login',
+    // use the storeReturnTo middleware to save the returnTo value from session to res.locals
+    storeReturnTo,
+    passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),
+    (req, res) => {
+        req.flash('success', 'Welcome back!');
+        const redirectUrl = res.locals.returnTo || '/campgrounds'; 
+        res.redirect(redirectUrl);
+    });
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+}); 
 
 module.exports = router

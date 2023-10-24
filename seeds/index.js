d7c40fc41b39f7c33fa3c76aa6805f941d37206d
Erik <kordylasinski.erik@gmail.com>
2023-10-24 13:27:20 -0500

    added mapbox for geo location to display a map when adding a campground based on user input

--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,10 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [-113.1331, 47.0202]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/dvkakrwbz/image/upload/v1697749488/Campy/lqzfkivdcced5mre5gtv.jpg',

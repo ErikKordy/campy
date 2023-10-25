cc427e704ca49d36b6b5c751e5b216f13a4424fe
Erik <kordylasinski.erik@gmail.com>
2023-10-25 09:54:35 -0500

    style clean-up

--- public/scripts/clusterMap.js
+++ public/scripts/clusterMap.js
@@ -1,11 +1,13 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-container: 'map',
+container: 'cluster-map',
 // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
 style: 'mapbox://styles/mapbox/dark-v11',
 center: [-103.5917, 40.6699],
 zoom: 3
 });
+
+map.addControl(new mapboxgl.NavigationControl())
  
 map.on('load', () => {
 // Add a new source from our GeoJSON data and
@@ -115,9 +117,7 @@ coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
  
 new mapboxgl.Popup()
 .setLngLat(coordinates)
-.setHTML(
-    popUpMarkup
-)
+.setHTML(popUpMarkup)
 .addTo(map);
 });
  

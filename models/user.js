a9209f9dd451f15b54d711e4dffa9c7d694a0bf2
Erik <kordylasinski.erik@gmail.com>
2023-10-26 10:48:50 -0500

    fixed username issue

--- models/user.js
+++ models/user.js
@@ -3,12 +3,15 @@ const Schema = mongoose.Schema
 const passportLocalMongoose = require('passport-local-mongoose')
 
 const userSchema = new Schema({
+    username: String, 
     email: {
         type: String,
         required: true,
         unique: true
     }
-})
-userSchema.plugin(passportLocalMongoose)
+});
+userSchema.plugin(passportLocalMongoose, {
+    usernameField: 'username', 
+});
 
 module.exports = mongoose.model('User', userSchema)
